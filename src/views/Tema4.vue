51448803868a226f192833af4d73b973900bb739
FreidiOrtiz <32910613+Freidi@users.noreply.github.com>
2022-10-10 23:11:24 -0500

    temas hasta el 5 terminados
    Se terminaron todos los temas incluyendo el 5 quedando pendiente la actividad didáctica, glosario y fuentes, pendiente video

--- src/views/Tema4.vue
+++ src/views/Tema4.vue
@@ -1,27 +1,111 @@
 <template lang="pug">
-  .curso-main-container.pb-3
-    BannerInterno
-    .container.tarjeta.tarjeta--blanca.p-4.p-md-5.mb-5
-      .titulo-principal.color-acento-contenido
-        .titulo-principal__numero
-          span 4
-        h1 Técnicas de recolección de información
-      .row.justify-content-center.mb-4
-        .col-md-5
-          figure.mw-sm-350px
-            img(src='@/assets/curso/temas/texto_destacado/img20.png', alt='imagen relacionada')
-        .col-md-7      
-          p Al atender a los clientes, uno de los aspectos más importantes es la recolección de información. Pero, ¿qué es esto y para qué sirve? 
-          p Las técnicas de recolección de información se usan para recoger y analizar diferentes formas de información. Estas incluyen examinar documentos sobre un tema específico o realizar entrevistas, observaciones y encuestas para producir información cualitativa o cuantitativa, a partir de la cual se pueden tomar decisiones.
-     
-      .row.justify-content-around.align-items-center.mb-4
-        .col-lg-9
-          .cajon.color-acento-botones.color-acento-botones.p-4.mb-4.mt-4 
-            p.mb-0 La información cualitativa de un evento empresarial describe características, cualidades y rasgos no cuantificables como opiniones personales, descripciones o percepciones sobre un tema determinado, mientras que la información cuantitativa es medible y corresponde a estadísticas, cantidades o mediciones.     
-   
-      p.mb-4 Existen varias formas de recolectar información según datos requeridos y actividades desarrolladas. De acuerdo con Harvard Business School, entre las técnicas de recolección de información más usadas en analítica empresarial se encuentran las siguientes: 
+.curso-main-container.pb-3
+  BannerInterno
+  .container.tarjeta.tarjeta--blanca.p-4.p-md-5.mb-5
+    .titulo-principal.color-acento-contenido
+      .titulo-principal__numero
+        span 4
+      h1 Técnicas de recolección de información
+    .row.justify-content-center.mb-4
+      .col-md-5
+        figure.mw-sm-350px
+          img(src='@/assets/curso/temas/texto_destacado/img20.png', alt='imagen relacionada')
+      .col-md-7      
+        p Al atender a los clientes, uno de los aspectos más importantes es la recolección de información. Pero, ¿qué es esto y para qué sirve? 
+        p Las técnicas de recolección de información se usan para recoger y analizar diferentes formas de información. Estas incluyen examinar documentos sobre un tema específico o realizar entrevistas, observaciones y encuestas para producir información cualitativa o cuantitativa, a partir de la cual se pueden tomar decisiones.
+    
+    .row.justify-content-around.align-items-center.mb-4
+      .col-lg-9
+        .cajon.color-acento-botones.color-acento-botones.p-4.mb-4.mt-4 
+          p.mb-0 La información cualitativa de un evento empresarial describe características, cualidades y rasgos no cuantificables como opiniones personales, descripciones o percepciones sobre un tema determinado, mientras que la información cuantitativa es medible y corresponde a estadísticas, cantidades o mediciones.     
   
-  </template>
+    p.mb-4 Existen varias formas de recolectar información según datos requeridos y actividades desarrolladas. De acuerdo con Harvard Business School, entre las técnicas de recolección de información más usadas en analítica empresarial se encuentran las siguientes: 
+    
+    .row.justify-content-center.mb-4
+        .col-lg-12
+          AcordionA(tipo="a" clase-tarjeta="tarjeta color-secundario")
+            .row(titulo="Observación")
+              .col-md-6.mb-4.mb-md-0
+                p Implica observar el comportamiento o actuar de un sujeto en un entorno específico para comprenderlo y registrar lo observado. Hoy en día, se observa a personas interactuar con productos, sitios web y servicios en tiempo real, muchas veces, sin que esas personas lo sepan.
+                
+              .col-6.mb-3
+                img(src='@/assets/curso/temas/texto_destacado/img21.png', alt='imagen relacionada')  
+           
+            .row(titulo="Entrevistas y grupos de discusión")
+              .col-md-6.mb-4.mb-md-0
+                p Son conversaciones grupales entre personas con intereses, características o necesidades comunes. En ambos casos, un facilitador hace preguntas que se debaten entre todos para obtener información sobre diferentes temas, percepciones, creencias y actitudes en un entorno social específico.
+               
+              .col-6.mb-3
+                img(src='@/assets/curso/temas/texto_destacado/img22.png', alt='imagen relacionada') 
+
+            .row(titulo="Seguimiento de transacciones")
+              .col-md-6.mb-4.mb-md-0
+                p Se refiere a la acción de acceder a datos de compras de clientes en sitios web, proveedores de servicios o comercio electrónico, que se analizan para generar estrategias de venta enfocadas en las preferencias de consumo.
+              
+              .col-6.mb-3
+                img(src='@/assets/curso/temas/texto_destacado/img23.png', alt='imagen relacionada') 
+
+            .row(titulo="Seguimiento en redes sociales")
+              .col-md-6.mb-4.mb-md-0
+                p Implica seguir posibles clientes para conocer su actividad en línea y comprender qué productos o servicios les interesan para generar anuncios adecuados y productos relevantes.
+               
+              .col-6.mb-3
+                img(src='@/assets/curso/temas/texto_destacado/img24.png', alt='imagen relacionada')
+            
+            .row(titulo="Seguimiento en línea")
+              .col-md-6.mb-4.mb-md-0
+                p Técnica con cookies (fragmentos de texto que un sitio web envía al navegador para recordar información sobre preferencias del visitante) por medio de la cual se recogen datos, anuncios en páginas web, campañas de correo electrónico, para analizar cifras, rastrear usuarios y su comportamiento en línea.
+                
+              .col-6.mb-3
+                img(src='@/assets/curso/temas/texto_destacado/img25.png', alt='imagen relacionada')
+
+            .row(titulo="Encuesta")
+              .col-md-6.mb-4.mb-md-0
+                p Puede ser física o digital. Esta recoge datos cuantitativos y cualitativos. Es económica, accesible a investigadores y usuarios, brinda información específica y constituye una base para mejorar o realizar nuevos estudios.
+               
+              .col-6.mb-3
+                img(src='@/assets/curso/temas/texto_destacado/img26.png', alt='imagen relacionada')
+
+            .row(titulo="Formulario")
+              .col-md-6.mb-4.mb-md-0
+                p Es más general que la encuesta, logra recopilar datos cualitativos, demográficos o de contacto y permite el seguimiento a clientes potenciales a fin de conocerlos mejor.
+                
+              .col-6.mb-3
+                img(src='@/assets/curso/temas/texto_destacado/img27.png', alt='imagen relacionada')
+
+    p También encontramos unas características que deben cumplir los diferentes instrumentos para recolección de información. Algunas de esas características son: 
+
+    .row.col-md-12.mb-4
+      .col-lg-5
+        figure
+            img(style="height: 655px" data-aos="fade-down-right" src='@/assets/curso/temas/texto_destacado/img28.png', alt='imagen relacionada')
+      .col-lg-6
+        AcordionA(tipo="a" clase-tarjeta="tarjeta color-secundario" style="width: 610px" )
+          .row(titulo="Ser completos y funcionales")
+            .col-md-12.mb-4.mb-md-4
+              p Es decir, deben ser compatibles con escenarios reales y permitir formular diferentes tipos de preguntas. Deben tener validación aplicable a diferentes opciones de respuesta para recolectar datos precisos y minimizar su limpieza, y deben incluir preguntas relevantes.
+              
+          .row(titulo="Ser fáciles de usar y amigables con los clientes")
+            .col-md-12.mb-4.mb-md-0
+            p Su diseño debe ser intuitivo, integrable con otras herramientas y visión global, para poder conectar con la administración de relaciones en las distintas etapas de CRM (Customer Relationship Management).
+           
+          .row(titulo="Simplificar la elaboración de informes")
+            .col-md-12.mb-4.mb-md-0
+            p En lo posible, deben aplicarse en línea, a través de una interfaz de usuario intuitiva que dé sentido a los datos obtenidos.
+            
+          .row(titulo="Estar diseñados para facilitar la atención al cliente")
+            .col-md-12.mb-4.mb-md-0
+              p Su política de atención al cliente y los can ales de comunicación deben ser útiles y constantes para resolver conflictos. Deben ofrecer diversas formas de contacto, estar diseñados en el idioma de quien lo va a diligenciar y prestar ayuda rápida y eficiente.
+              
+          .row(titulo="Tener buena relación de costo-beneficio")
+            .col-md-12.mb-4.mb-md-0
+            p Este es un parámetro fundamental al escoger un instrumento para recolección de información. 
+    
+    h5 Instrumentos de recolección de información
+
+    p Dados los criterios para recolectar información tales como la naturaleza del objeto de estudio, la posibilidad de acceso a la información, el tamaño de la muestra, los recursos disponibles, la facilidad para obtener datos, el tipo y la naturaleza de la fuente de datos, surgen instrumentos como los cuestionarios (conjunto de preguntas que permiten recoger, procesar y analizar información pertinente) y las escalas de actitudes (instrumento de evaluación para recoger información específica), que se construyen con preguntas sobre variables a medir según objetivos de investigación. 
+
+</template>
 
 <script>
 export default {
--- src/views/Tema5.vue
+++ src/views/Tema5.vue
@@ -1,13 +1,108 @@
 <template lang="pug">
-  .curso-main-container.pb-3
-    BannerInterno
-    .container.tarjeta.tarjeta--blanca.p-4.p-md-5.mb-5
-      .titulo-principal.color-acento-contenido
-        .titulo-principal__numero
-          span 3
-        h1 Titulo de primer nivel
+.curso-main-container.pb-3
+  BannerInterno
+  .container.tarjeta.tarjeta--blanca.p-4.p-md-5.mb-5
+    .titulo-principal.color-acento-contenido
+      .titulo-principal__numero
+        span 5
+      h1 Trazabilidad del servicio
+    .row.justify-content-center.mb-4
+        .col-md-5
+          figure.mw-sm-350px
+            img(src='@/assets/curso/temas/texto_destacado/img29.png', alt='imagen relacionada')
+        .col-md-7      
+          p Dada la importancia que tienen los eventos empresariales como medio de comunicación y fidelización de clientes internos y externos, es importante recalcar que el éxito de estos se debe a la correcta aplicación de cada uno de sus elementos. Dentro de estos elementos destaca la trazabilidad como un mecanismo de planeación, seguimiento, control y evaluación. Para conocer más sobre este tema, lo invitamos a ver el siguiente video.
+    br
+    figure.mb-4
+      .video
+        <iframe width="560" height="315" src="https://www.youtube.com/embed/jASywqOHrQM" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+    br
+
+    h3.mb-4.mt-4
+      i#t_5_1.fas.fa-square.me-3.cuadrado
+      | Informes de servicio
+
+    .row.justify-content-center.mb-4
+        .col-md-5
+          figure.mw-sm-350px
+            img(data-aos="fade-down-right" src='@/assets/curso/temas/texto_destacado/img30.png', alt='imagen relacionada')
+        .col-md-7      
+          p Por otra parte, un informe de servicio recopila información inherente de los procesos que hacen parte del ciclo de servicios y la transforma en datos numéricos, que permiten aplicar un análisis sobre los resultados o la percepción del servicio.
+
+    p.mb-4 Conozcamos unos datos importantes sobre los informes de servicios: 
+
+    .row.mb-5
+      .col-sm-6.col-lg-6.col-xl-4.mb-4.mb-xl-0
+        .tarjeta.tarjeta-flip.color-primario(@mouseover="indicadorTarjetaFlip = false")
+          .tarjeta-flip__contenedor
+            .tarjeta-flip__img(:style="{'background-image': `url(${require('@/assets/curso/temas/texto_destacado/img31.svg')})`}")
+              .titulo-principal__numero.circulo 
+               span 1
+            .tarjeta-flip__contenido.p-4.p-xl-5(style="text-align: center")
+              p Un buen informe de servicio al cliente toma datos sin procesar y los convierte en estadísticas, para ofrecer una visión general de lo que necesitan y esperan los clientes. Por tanto, este permitirá que la empresa haga seguimiento a tendencias, identifique áreas por mejorar, planifique con eficiencia horarios, carga laboral del personal y pueda mejorar su oferta de servicios.
+
+      .col-sm-6.col-lg-6.col-xl-4.mb-4.mb-sm-0
+        .tarjeta.tarjeta-flip.color-primario(@mouseover="indicadorTarjetaFlip = false")
+          .tarjeta-flip__contenedor
+            .tarjeta-flip__img(:style="{'background-image': `url(${require('@/assets/curso/temas/texto_destacado/img32.svg')})`}")
+              .titulo-principal__numero.circulo 
+                 span 2
+            .tarjeta-flip__contenido.p-4.p-xl-5(style="text-align: center")
+              p Un informe de servicio al cliente facilita a una empresa el proceso de toma de decisiones, permite aplicar acciones de mejora frente al nivel de satisfacción de los clientes, contribuye a lograr una mejor gestión de la fuerza laboral y mejora los niveles de eficacia y productividad del equipo de atención al cliente. Está claro que, en la actualidad, el consumidor está más conectado y empoderado, debido a que tiene conocimientos digitales. Por lo tanto, este es más exigente frente a la calidad del servicio que espera de las marcas que usa.
+
+      .col-sm-6.col-lg-6.col-xl-4
+        .tarjeta.tarjeta-flip.color-primario(@mouseover="indicadorTarjetaFlip = false")
+          .tarjeta-flip__contenedor
+            .tarjeta-flip__img(:style="{'background-image': `url(${require('@/assets/curso/temas/texto_destacado/img33.svg')})`}")
+              .titulo-principal__numero.circulo 
+                 span 3
+            .tarjeta-flip__contenido.p-4.p-xl-5(style="text-align: center")
+              p Las estadísticas de servicio al cliente, las cuales son utilizadas en el informe de servicio, permiten a una empresa medir su desempeño, eficiencia y muestran qué canales de asistencia prefieren usar los clientes. Además, hacen posible estimar la cantidad de personal que se requiere para atender el área de servicio al cliente de forma efectiva, detectar si el equipo de trabajo cumple con sus funciones de manera adecuada, mejorar la carga laboral para evitar agotamiento, identificar y recompensar el buen desempeño, dar información relevante para mejorar productividad y conocer la información detallada sobre percepción del cliente.
+      
+    p.mb-4.mt-4 Para finalizar, es importante recordar que un servicio es la acción o el conjunto de actividades destinadas a satisfacer una necesidad determinada de un cliente con un producto intangible, personalizado y heterogéneo, porque atiende diferentes factores; es decir, no involucra cadenas de producción en masa como sí ocurre en la producción de bienes. 
+    
+    p.mb-4 Las características más importantes de los servicios son:
+
+    TabsA.color-acento-contenido.mb-4
+        .tarjeta.color-acento-contenido-borde.p-4(titulo="Intangibilidad")
+          .row
+            .col-xl-6.p-4.color-fondo
+              h4 Intangibilidad 
+              p No se pueden ver ni tocar. 
+            .col-xl-6.mb-4
+              figure
+                img(data-aos="fade-up" src='@/assets/curso/temas/texto_destacado/img34.svg', alt='imagen relacionada')
+        .tarjeta.color-acento-contenido-borde.p-4(titulo="Indivisibilidad")
+          .row
+            .col-lg-.col-xl-6.p-4.color-fondo
+              h4 Indivisibilidad
+              p Aunque se venden paquetes de servicios, hay una unidad mínima indivisible. 
+            .col-xl-6
+              figure
+                img(data-aos="fade-up" src='@/assets/curso/temas/texto_destacado/img34.svg', alt='imagen relacionada')
+        .tarjeta.color-acento-contenido-borde.p-4(titulo="Democrático")
+          .row
+            .col-xl-6.p-4.color-fondo
+              h4 Heterogeneidad
+              p Son diversos y variados.
+            .col-xl-6
+              figure
+                img(data-aos="fade-up" src='@/assets/curso/temas/texto_destacado/img34.svg', alt='imagen relacionada')
+        .tarjeta.color-acento-contenido-borde.p-4(titulo="Consultivo")
+          .row
+            .col-xl-6.p-4.color-fondo
+              h4 No conservables
+              p No se pueden almacenar. La prestación del servicio y el consumo ocurren simultáneamente. Por ejemplo, cuando un mesero atiende en un restaurante, no se puede almacenar o guardar ese servicio. Si un pasaje de avión con fecha cerrada no se usa, se pierde, pues el vuelo se realiza en la fecha y la hora programadas; puede volar en otro momento, pero no se puede repetir ese instante.
+            .col-xl-6.mb-4
+              figure(style="margin-top: 50px;")
+                img(data-aos="fade-up" src='@/assets/curso/temas/texto_destacado/img34.svg', alt='imagen relacionada')
+
+    .row.justify-content-around.align-items-center.mb-4
+      .col-lg-9
+        .cajon.color-acento-botones.color-fondo-secundario.p-4.mb-4.mt-4 
+          p.mb-0 Recuerde explorar los demás recursos que se encuentran disponibles en este componente formativo; para ello, diríjase al menú principal, donde encontrará la síntesis, una actividad didáctica para reforzar los conceptos estudiados, material complementario, entre otros.     
   
-  </template>
+</template>
 
 <script>
 export default {
