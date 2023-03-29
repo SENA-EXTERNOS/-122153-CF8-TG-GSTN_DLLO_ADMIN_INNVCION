9bbdbd9126048636b68e55e94be242d08b5c860e
ricardog07 <71521082+ricardog07@users.noreply.github.com>
2023-03-28 21:29:34 -0500

    Se vinculan los videos.

--- src/views/Tema5.vue
+++ src/views/Tema5.vue
@@ -15,7 +15,7 @@
     br
     figure.mb-4
       .video
-        <iframe width="560" height="315" src="https://www.youtube.com/embed/jASywqOHrQM" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+        <iframe width="560" height="315" src="https://www.youtube.com/embed/zzn6pjLBW5Q" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
     br
 
     h3.mb-4.mt-4
